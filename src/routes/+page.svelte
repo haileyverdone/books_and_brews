049fcc5fa7919c0bb258a514e9163eeb2069ce24
Hailey Verdone <haileyverdone@gmail.com>
2024-10-18 00:46:57 -0700

    update on home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,50 @@
-<script>
-    // Any JavaScript code can go here, but it's fine to leave this empty
-  </script>
-  
-  <!-- Bootstrap Content -->
-  <div class="container mt-5">
+<div id="myCarousel" class="carousel slide" data-bs-ride="carousel" data-bs-interval="2000">
+  <ol class="carousel-indicators">
+    <li data-bs-target="#myCarousel" data-bs-slide-to="0" class="active"></li>
+    <li data-bs-target="#myCarousel" data-bs-slide-to="1"></li>
+    <li data-bs-target="#myCarousel" data-bs-slide-to="2"></li>
+  </ol>
+<div class="carousel-inner " role="listbox">
+    <div class="carousel-item active" style="background-color: #ff9999;">
+      <div class="carousel-content">
+      </div>
+    </div>
+    <div class="carousel-item" style="background-color: #99ccff;">
+      <div class="carousel-content">
+      </div>
+    </div>
+    <div class="carousel-item" style="background-color: #99ff99;">
+      <div class="carousel-content">
+      </div>
+    </div>
   </div>
-  
+</div>
+<a class="carousel-control-left" href="#myCarousel" role="button" data-bs-slide="prev">
+  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+  <span class="visually-hidden">Previous</span>
+</a>
+<a class="carousel-control-right" href="#myCarousel" role="button" data-bs-slide="next">
+  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+  <span class="visually-hidden">Next</span>
+</a>
+
+<style>
+  /* Styles for your carousel */
+  .carousel-inner {
+    height: 500px;
+    position: relative;
+  }
+
+  .carousel-inner{
+    position: relative;
+  }
+
+  .carousel-item {
+    align-items: center;
+    height: 500px;
+    background-color: beige;
+  }
+  .carousel-indicators{
+    list-style: none;
+  }
+</style>
