d2b3c59f26c53075b0644c69dad20b553cb36eb9
Hailey Verdone <haileyverdone@gmail.com>
2024-10-21 23:34:04 -0700

    testing

--- src/routes/explore/+page.svelte
+++
@@ -1,2 +0,0 @@
-
-<h3>Explore Page</h3>
--- svelte.config.js
+++
@@ -1,9 +0,0 @@
-import adapter from '@sveltejs/adapter-auto'; // Make sure the adapter is installed
-import preprocess  from 'svelte-preprocess'; // Import preprocess
-
-export default {
-  preprocess: preprocess(), // Use preprocess for Svelte files
-  kit: {
-    adapter: adapter(), // Use the adapter for your deployment
-  },
-};
